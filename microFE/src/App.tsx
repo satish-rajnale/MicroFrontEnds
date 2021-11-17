dead8a46de5768ec690eb11585af73b6f5c6c259
satish-rajnale <satishrajnale98@gmail.com>
2021-11-17 22:11:30 +0530

    config

--- microFE/src/App.tsx
+++ microFE/src/App.tsx
@@ -1,14 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom";
-
-import "./index.scss";
+import React from 'react';
+import ReactDOM from 'react-dom';
+//@ts-ignore
+import Counter from 'remoteFE/Counter';
+import './index.scss';
 
 const App = () => (
   <div className="mt-10 text-3xl mx-auto max-w-6xl">
     <div>Name: microFE</div>
-    <div>Framework: react</div>
-    <div>Language: TypeScript</div>
-    <div>CSS: Tailwind</div>
+    <Counter />
   </div>
 );
-ReactDOM.render(<App />, document.getElementById("app"));
+ReactDOM.render(<App />, document.getElementById('app'));
