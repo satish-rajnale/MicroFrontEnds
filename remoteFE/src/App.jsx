c7218b56abf1f5536d52fec4c26c547ff61e2368
satish-rajnale <satishrajnale98@gmail.com>
2021-11-17 21:53:22 +0530

    exposing app

--- remoteFE/src/App.jsx
+++ remoteFE/src/App.jsx
@@ -1,13 +1,11 @@
-import { render } from "solid-js/web";
-
-import "./index.scss";
+import { render } from 'solid-js/web';
+import Counter from './Counter';
+import './index.scss';
 
 const App = () => (
   <div class="mt-10 text-3xl mx-auto max-w-6xl">
     <div>Name: remoteFE</div>
-    <div>Framework: solid-js</div>
-    <div>Language: JavaScript</div>
-    <div>CSS: Tailwind</div>
+    <Counter />
   </div>
 );
-render(App, document.getElementById("app"));
+render(App, document.getElementById('app'));
