a3d6024a86ac20842f995c3dbf49906e557651c7
satish-rajnale <satishrajnale98@gmail.com>
2021-11-27 22:41:55 +0530

    Update App.tsx

--- reactApp/src/App.tsx
+++ reactApp/src/App.tsx
@@ -6,15 +6,21 @@ import { useClient } from './host';
 import './index.scss';
 
 const App = () => {
-  const { login, logout, JWT } = useClient();
+  const { login, logout, JWT, getTodos } = useClient();
 
   const [name, setName] = useState('sally');
   const [password, setPassword] = useState('123');
+  const [todos, setTodos] = useState([]);
 
   const onLogin = useCallback(async () => {
     login(name, password);
   }, [name, password]);
   const divRef = useRef();
+
+  const onGetTodos = useCallback(async () => {
+    // queryClient.invalidateQueries('todos');
+    setTodos(await getTodos());
+  }, [getTodos]);
   // useEffect(() => {
   //   CounterWrapper(divRef.current);
   // }, []);
@@ -54,6 +60,25 @@ const App = () => {
             </>
           )}
         </div>{' '}
+        <div>
+          {JWT && todos && (
+            <>
+              <div>
+                {todos.map((todo) => (
+                  <div key={todo}>{todo}</div>
+                ))}
+              </div>
+              <div className="mt-10">
+                <button
+                  onClick={onGetTodos}
+                  className="px-5 py-2 bg-blue-600 text-white rounded-xl"
+                >
+                  Get Todos
+                </button>
+              </div>
+            </>
+          )}
+        </div>
       </div>
     </div>
   );
